3ee4949b284230c0e6c9718010b34ddbc75d8672
Benjamin Held <me@bheld.eu>
2021-01-10 20:29:06 +0100

    refactored script output

--- migration/wrf_migration.sh
+++ migration/wrf_migration.sh
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2020-09-05 20:15:59
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-10-25 14:27:01
+# @Last Modified time: 2021-01-10 20:17:09
 
 # Migration-Script (currently in testing) from one WRF version to another
 # The versions and folders need to be set manually at the moment
@@ -25,18 +25,18 @@ yay -S --noconfirm --needed libpciaccess libunwind
 FILE_NAME_OLD='wrf_410'; WRF_FOLDER_OLD='WRF-4.1.5'; WPS_FOLDER_OLD='WPS-4.1'
 FILE_NAME='wrf_420'; WRF_FOLDER='WRF-4.2'; WPS_FOLDER='WPS-4.2'
 
-# deploying new archive
+# deploy new archive
 cd ${HOME} || exit 1
 URL_PATH="https://bheld.eu/data/wrf_deploy/${FILE_NAME}.tar.gz"
 ARCHIVE="${FILE_NAME}.tar.gz"
 printf "%b\\nLoading wrf archive: %b\\n" "${YELLOW}" "${NC}"
 wget "${URL_PATH}"
-printf "%b\\nUnpacking archive: %b\\n" "${YELLOW}" "${NC}"
+printf "%bUnpacking archive... %b\\n" "${YELLOW}" "${NC}"
 tar -xzf "${ARCHIVE}"
 rm "${ARCHIVE}"
 
 # copy old configuration files
-printf "%b\\nDeploying repository config files: %b\\n" "${YELLOW}" "${NC}"
+printf "%bDeploying repository config files... %b\\n" "${YELLOW}" "${NC}"
 cp "${HOME}/${FILE_NAME_OLD}/${WPS_FOLDER_OLD}/namelist.wps" "${HOME}/${FILE_NAME}/${WPS_FOLDER}"
 cp "${HOME}/${FILE_NAME_OLD}/${WRF_FOLDER_OLD}/test/em_real/namelist.input" "${HOME}/${FILE_NAME}/${WRF_FOLDER}/test/em_real/"
 cp "${HOME}/${FILE_NAME_OLD}/${WRF_FOLDER_OLD}/test/em_real/tslist" "${HOME}/${FILE_NAME}/${WRF_FOLDER}/test/em_real/"
