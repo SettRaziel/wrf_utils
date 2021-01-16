c4b4a03a81208ba402fe5b09e2b843b0e5094066
Benjamin Held <me@bheld.eu>
2021-01-16 18:49:38 +0100

    updated script call syntax

--- cron_job/prepare_cron.sh
+++ cron_job/prepare_cron.sh
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2017-04-04 19:57:11
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-04-27 16:37:16
+# @Last Modified time: 2021-01-16 18:49:13
 
 # setting -e to abort on error
 set -e
@@ -18,7 +18,7 @@ sudo systemctl enable cronie.service
 # start cron service
 sudo systemctl start cronie.service
 
-printf "%bDefine crontabs, e.g. daily modelrun for 18 UTC data at midnight\\n%b" "${YELLOW}" "${NC}"
-printf "%bcrontab -u http -e 00 00 * * * /abs_path/to/run_model.sh 18\\n%b" "${YELLOW}" "${NC}"
+printf "%bDefine crontabs, e.g. daily modelrun for 18 UTC data 36h forecast at 0.5 degree at midnight\\n%b" "${YELLOW}" "${NC}"
+printf "%bcrontab -u http -e 00 00 * * * /abs_path/to/run_model.sh -h 18 -p 36 -r 0p50\\n%b" "${YELLOW}" "${NC}"
 
 crontab -l
