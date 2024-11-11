56f618c7f5778dd236d6012b8274f439830bdfbc
Benjamin Held <me@bheld.eu>
2024-11-11 20:25:45 +0100

    add script to check gems after ruby update

---
+++ update/gem_update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+. ../libs/terminal_color.sh
+
+# error handling for input parameter
+if [ "$#" -ne 1 ]; then
+  printf "%bWrong number of arguments. Must be one for <DESTINATION>.%b\\n" "${RED}" "${NC}"
+  exit 1
+fi
+
+DESTINATION_PATH="${1}"
+
+# check if directory exists and change to it
+if ! [ -d "${DESTINATION_PATH}" ]; then
+  cd "${DESTINATION_PATH}"
+fi
+
+# upgrade bundler dependencies
+bundle update
+
+# install for specific_install gem
+gem install specific_install
+
+# check wrf dependencies
+gem specific_install -l https://github.com/SettRaziel/ruby_utils.git
+gem specific_install -l https://github.com/SettRaziel/wrf_library.git
+gem specific_install -l https://github.com/SettRaziel/wrf_forecast.git
+
+# update gems
+gem update
