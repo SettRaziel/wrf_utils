6ca96e4b4b117f3c82d0403bcc2238a71b047b1f
Benjamin Held <me@bheld.eu>
2025-05-17 18:36:02 +0200

    add migration script to wrf_460

---
+++ migration/to_460/wrf_migration_460.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Migration-Script (currently in testing) from WRF version 4.4.0 to 4.5.0
+
+# enable termination on error
+set -e
+
+# define terminal colors
+. ../../libs/terminal_color.sh
+
+SCRIPT_PATH=$(pwd)
+
+# dependency required with WRF 4.2.0
+yay -S --noconfirm --needed libpciaccess libunwind
+
+# packages required with WRF 4.4.0
+yay -S --noconfirm --needed zlib libpng jasper
+
+# no package changes for WRF 4.5.0
+
+# compatibility for WRF model due to libxml2 update from may 2025
+yay -S --noconfirm --needed libxml2-legacy
+
+# old folder names and new one; adjust to WRF 4.2.0 if required
+FILE_NAME_OLD='wrf_450'; WRF_FOLDER_OLD='WRF-4.5'; WPS_FOLDER_OLD='WPS-4.5'
+FILE_NAME='wrf_460'; WRF_FOLDER='WRF-4.6.1'; WPS_FOLDER='WPS-4.6.0'
+
+# deploy new archive
+cd ${HOME} || exit 1
+URL_PATH="https://bheld.eu/data/wrf_deploy/${FILE_NAME}.tar.gz"
+ARCHIVE="${FILE_NAME}.tar.gz"
+printf "%b\\nLoading wrf archive: %b\\n" "${YELLOW}" "${NC}"
+wget "${URL_PATH}"
+printf "%bUnpacking archive... %b\\n" "${YELLOW}" "${NC}"
+tar -xzf "${ARCHIVE}"
+rm "${ARCHIVE}"
+
+# copy old configuration files
+printf "%bDeploying repository config files... %b\\n" "${YELLOW}" "${NC}"
+cp "${HOME}/${FILE_NAME_OLD}/${WPS_FOLDER_OLD}/namelist.wps" "${HOME}/${FILE_NAME}/${WPS_FOLDER}"
+cp "${HOME}/${FILE_NAME_OLD}/${WRF_FOLDER_OLD}/test/em_real/namelist.input" "${HOME}/${FILE_NAME}/${WRF_FOLDER}/test/em_real/"
+cp "${HOME}/${FILE_NAME_OLD}/${WRF_FOLDER_OLD}/test/em_real/tslist" "${HOME}/${FILE_NAME}/${WRF_FOLDER}/test/em_real/"
