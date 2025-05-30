2b04d8f9fa29a63f4d8391446de1f39a44071ecf
Benjamin Held <me@bheld.eu>
2025-05-30 17:58:40 +0200

    add script to start model run from yesterday

---
+++ cron_job/start_wrf_yesterday.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# setting -e to abort on error
+set -e
+
+. ../libs/terminal_color.sh
+
+# parameter check
+if [ "$#" -ne 1 ]; then
+  printf "%bWrong number of arguments. Must be one for <HOUR>.%b\\n" "${RED}" "${NC}"
+  exit 1
+fi
+
+YEAR=$(date -u +%Y -d "yesterday")
+MONTH=$(date -u +%m -d "yesterday")
+DAY=$(date -u +%d -d "yesterday")
+HOUR=${1}
+
+sh /home/raziel/wrf_archlinux/wrf_run/run_model.sh "-b" "wrf_450" "-y" "${YEAR}" "-m" "${MONTH}" "-d" "${DAY}" "-h" "${HOUR}" "-p" "96" "-r" "0p50"
