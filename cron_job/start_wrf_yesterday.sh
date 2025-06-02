5afdcccdc86b5f397b2fef7d365e27d72e3c2592
Benjamin Held <me@bheld.eu>
2025-06-02 08:55:23 +0200

    use local time zone for date evaluation

--- cron_job/start_wrf_yesterday.sh
+++ cron_job/start_wrf_yesterday.sh
@@ -11,9 +11,9 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-YEAR=$(date -u +%Y -d "yesterday")
-MONTH=$(date -u +%m -d "yesterday")
-DAY=$(date -u +%d -d "yesterday")
+YEAR=$(date +%Y -d "yesterday")
+MONTH=$(date +%m -d "yesterday")
+DAY=$(date +%d -d "yesterday")
 HOUR=${1}
 
 sh "${HOME}/wrf_archlinux/wrf_run/run_model.sh" "-b" "wrf_450" "-y" "${YEAR}" "-m" "${MONTH}" "-d" "${DAY}" "-h" "${HOUR}" "-p" "96" "-r" "0p50"
